d1cf1ec60afda7f2431f7765882cd4a318a235df
Huy <h.qt193@gmail.com>
2023-03-17 19:23:08 -0400

    add github and linkedin url

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,20 @@
-
+import { Nav } from 'react-bootstrap'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
 
 const About = () => {
     return (
         <>  
             <div className="mt-5">
                 <h2 className='about text-5xl font-bold' id="about">Huy Tran</h2>
+                <br />
+                <div className="flex flex-row gap-20 justify-center">
+                    <Nav.Link target="_blank" href="https://github.com/huy-tran49"><BsGithub size={30}/></Nav.Link>
+                    <Nav.Link target="_blank" href="https://www.linkedin.com/in/huy-tran-5278b6126/"><BsLinkedin size={30}/></Nav.Link>
+                </div>
+                <br />
                     <section>
                         <p>
-                            Hello, I'm a software engineer.
+                            Hello, I'm a software engineer and basketball enthusiast locate in Fairfax, Virginia. Previously worked in IT, I'm looking to combine my passion in technology to solve real world issues.
                         </p>
                     </section>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'mint-green': '#73AB84',
+        'lt-purple': '#777898',
+      }
+    },
   },
   plugins: [],
 }
